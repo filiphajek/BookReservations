2458f81a3417b69ebb3fe776d1917486cd5371ac
Filip Hájek <filip.hajek@notino.com>
2023-05-08 18:19:41 +0200

    db migration

--- api/src/BookReservations.Api.DAL/Migrations/20230508160448_Init.Designer.cs
+++ api/src/BookReservations.Api.DAL/Migrations/20230508160448_Init.Designer.cs
@@ -4,6 +4,7 @@ using BookReservations.Api.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookReservations.Api.DAL.Migrations
 {
     [DbContext(typeof(BookReservationsDbContext))]
-    partial class BookReservationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230508160448_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
